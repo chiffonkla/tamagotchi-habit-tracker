6df643ef66d030d03598e8dc88a062e001d0d24c
chiffonkla <116045907+chiffonkla@users.noreply.github.com>
2025-10-14 22:24:21 -0700

    Update index.py

--- backend/index.py
+++ backend/index.py
@@ -2859,5 +2859,6 @@ def logout():
         db.close()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
 
